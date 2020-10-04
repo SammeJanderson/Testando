3378b54d7d34e6a57f180da5ba0f08f1081323e6
Samme Janderson <samme.san@gmail.com>
2020-10-04 12:05:18 -0300

    Snake

--- Joguinhos/Snake/README.md
+++ Joguinhos/Snake/README.md
@@ -1,1 +1,2 @@
-# Snake
+# JavaSnake
+Um jogo da cobrinha codificado usando a API 2D do Java
---
+++ Joguinhos/Snake/src/resources/Main.java
@@ -0,0 +1,54 @@
+package resources;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+        public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> inputs = new ArrayList<>();
+
+
+        System.out.println(st.());
+
+
+
+
+
+//        int coordernadaUm = scanner.nextInt();
+//        int coordenadaDois = scanner.nextInt();
+//
+//        String vl1 = "0";
+//        String vl2 = "0";
+//
+//        if (coordernadaUm > 0){
+//            vl1="1";
+//        }
+//        if (coordenadaDois > 0){
+//            vl2="1";
+//        }
+//
+//        String n = vl1+vl2;
+//
+//        switch (n){
+//            case "11":
+//                System.out.println("primeiro");
+//                break;
+//            case "10":
+//                System.out.println("segundo");
+//                break;
+//            case "00":
+//                System.out.println("terceiro");
+//                break;
+//            case "01":
+//                System.out.println("quarto");
+//                break;
+//        }
+    }
+}
